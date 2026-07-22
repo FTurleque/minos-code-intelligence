3525030885affec3f2adac54b29cd20be0708852
Turleque Fabrice <57303912+FTurleque@users.noreply.github.com>
2026-07-22 23:55:24 +0200

    feat(m1): model indexer qualification

---
+++ src/main/java/com/minos/orchestration/IndexerQualification.java
@@ -0,0 +1,10 @@
+package com.minos.orchestration;
+
+/**
+ * Niveau de qualification d'un indexeur dans MINOS.
+ */
+public enum IndexerQualification {
+    QUALIFIED,
+    QUALIFIED_WITH_CONSTRAINTS,
+    EXPERIMENTAL
+}
