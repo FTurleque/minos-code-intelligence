532fdedaa2ebfb04acb3059d4bbdaadaa8a14443
Turleque Fabrice <57303912+FTurleque@users.noreply.github.com>
2026-07-22 11:44:39 +0200

    feat(domain): ajouter les types de symboles

---
+++ src/main/java/io/github/fturleque/minos/domain/SymbolKind.java
@@ -0,0 +1,24 @@
+package io.github.fturleque.minos.domain;
+
+/**
+ * Taxonomie commune minimale des symboles MINOS.
+ */
+public enum SymbolKind {
+    CLASS,
+    INTERFACE,
+    RECORD,
+    STRUCT,
+    ENUM,
+    ANNOTATION,
+    TRAIT,
+    METHOD,
+    CONSTRUCTOR,
+    FUNCTION,
+    FIELD,
+    PROPERTY,
+    VARIABLE,
+    TYPE_ALIAS,
+    NAMESPACE,
+    PACKAGE,
+    OTHER
+}
