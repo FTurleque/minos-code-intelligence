bc1012c6f0f675e004f2698873a0b77e203af4f8
Turleque Fabrice <57303912+FTurleque@users.noreply.github.com>
2026-07-22 11:44:06 +0200

    feat(domain): ajouter les types d'entités de code

---
+++ src/main/java/io/github/fturleque/minos/domain/CodeEntityType.java
@@ -0,0 +1,11 @@
+package io.github.fturleque.minos.domain;
+
+/**
+ * Type d'entité pouvant participer à une relation MINOS.
+ */
+public enum CodeEntityType {
+    PROJECT,
+    MODULE,
+    SOURCE_FILE,
+    SYMBOL
+}
