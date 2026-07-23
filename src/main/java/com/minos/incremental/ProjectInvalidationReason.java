65b1acd652cfd3070334148e835d1cf05889fb27
Turleque Fabrice <57303912+FTurleque@users.noreply.github.com>
2026-07-23 22:38:10 +0200

    M7.4 treat unreadable fingerprint baseline as full invalidation

--- src/main/java/com/minos/incremental/ProjectInvalidationReason.java
+++ src/main/java/com/minos/incremental/ProjectInvalidationReason.java
@@ -1,11 +1,12 @@
 package com.minos.incremental;
 
 /**
- * Motif explicable ayant conduit à une portée d'invalidation M7.3.
+ * Motif explicable ayant conduit à une portée d'invalidation M7.
  */
 public enum ProjectInvalidationReason {
     NO_ACTIVE_INDEX,
     MISSING_FINGERPRINT_BASELINE,
+    FINGERPRINT_BASELINE_UNREADABLE,
     BASELINE_INDEX_MISMATCH,
     BUILD_DEFINITION_CHANGED,
     IGNORE_POLICY_CHANGED,
