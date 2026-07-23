9ec3c8413256f0961e92b90664e5690538f3e320
Turleque Fabrice <57303912+FTurleque@users.noreply.github.com>
2026-07-23 19:59:32 +0200

    M6.6 add technology categories

---
+++ src/main/java/com/minos/architecture/ArchitectureTechnologyCategory.java
@@ -0,0 +1,10 @@
+package com.minos.architecture;
+
+/**
+ * Catégories de technologies que MINOS sait actuellement observer factuellement
+ * via la découverte de projet.
+ */
+public enum ArchitectureTechnologyCategory {
+    LANGUAGE,
+    BUILD_SYSTEM
+}
