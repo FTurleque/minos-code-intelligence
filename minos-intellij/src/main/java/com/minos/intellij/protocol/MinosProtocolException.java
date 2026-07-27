c75fa332ac702bba9f24f756ebc59ef2131bf00c
Turleque Fabrice <57303912+FTurleque@users.noreply.github.com>
2026-07-27 10:45:21 +0200

    Allow typed MINOS protocol failures

--- minos-intellij/src/main/java/com/minos/intellij/protocol/MinosProtocolException.java
+++ minos-intellij/src/main/java/com/minos/intellij/protocol/MinosProtocolException.java
@@ -1,6 +1,6 @@
 package com.minos.intellij.protocol;
 
-public final class MinosProtocolException extends Exception {
+public class MinosProtocolException extends Exception {
     public MinosProtocolException(String message) {
         super(message);
     }
